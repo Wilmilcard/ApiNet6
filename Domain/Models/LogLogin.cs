b251c0f55f7e16f91655a4d54fe9be7053d7376c
juan leon <leonjuandavid@hotmail.com>
2024-03-10 13:09:15 -0500

    log login

---
+++ Domain/Models/LogLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class LogLogin : Auditory
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [Column("LogLoginID")]
+        public int LogLoginID { set; get; }
+        public int UserID { get; set; }
+        public virtual User User { get; set; }
+    }
+}

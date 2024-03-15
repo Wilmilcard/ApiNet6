0fc42e629a1e41e3185593b8c78ec705689bdde3
juan leon <leonjuandavid@hotmail.com>
2024-03-15 17:14:01 -0500

    new Endpoint

--- ApiNet6/Controllers/AlquilerController.cs
+++ ApiNet6/Controllers/AlquilerController.cs
@@ -23,34 +23,6 @@ namespace ApiNet6.Controllers
             _context = context;
         }
 
-        //[HttpGet("[Action]")]
-        //public async Task<IActionResult> GetAll()
-        //{
-        //    try
-        //    {
-        //        var positions = _alquilerService.QueryNoTracking().OrderBy(x => x.Fecha_Reservacion);
-
-        //        var response = new
-        //        {
-        //            success = true,
-        //            data = positions
-        //        };
-
-        //        return new OkObjectResult(response);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        var response = new
-        //        {
-        //            success = false,
-        //            error = ex.Message,
-        //            errorCode = ex.HResult,
-        //            InnerError = ex.InnerException
-        //        };
-        //        return new BadRequestObjectResult(response);
-        //    }
-        //}
-
         [HttpGet("[Action]")]
         public async Task<IActionResult> GetAll()
         {
@@ -100,6 +72,70 @@ namespace ApiNet6.Controllers
                 var response = new
                 {
                     timer = sw.ElapsedMilliseconds / 1000.0,
+                    success = true,
+                    data = query,
+                };
+                return new OkObjectResult(response);
+            }
+            catch (Exception ex)
+            {
+                var response = new
+                {
+                    success = false,
+                    error = ex.Message,
+                    errorCode = ex.HResult
+                };
+                return new BadRequestObjectResult(response);
+            }
+        }
+
+        [HttpGet("[Action]/{id}")]
+        public async Task<IActionResult> GetByGameId([FromRoute]int id)
+        {
+            try
+            {
+                //var list = _context.AlquilerDets.Where(x => x.JuegoId == id).Select(x => x.AlquilerDetId).ToList();
+
+                var query = _alquilerService
+                    .QueryNoTracking()
+                    .Include(x => x.Cliente)
+                    .Include(x => x.Estado)
+                    .Include(x => x.AlquilerDets)
+                        .ThenInclude(j => j.Juego)
+                            .ThenInclude(j => j.Director)
+                    .Include(x => x.AlquilerDets)
+                        .ThenInclude(j => j.Juego)
+                            .ThenInclude(p => p.PlataformaJuegos)
+                                .ThenInclude(pj => pj.Plataforma)
+                    .Select(x => new
+                    {
+                        x.AlquilerId,
+                        x.Cliente.Nombre,
+                        x.Cliente.Apellido,
+                        x.Cliente.Telefono,
+                        x.Cliente.Nit,
+                        x.Cliente.Email,
+                        x.Cliente.Nacimiento,
+                        x.Estado.EstadoId,
+                        EstadoActual = x.Estado.Nombre,
+                        x.Fecha_Devolucion,
+                        x.Fecha_Reservacion,
+                        x.Valor_Total,
+                        AlquilerDets = x.AlquilerDets.Where(ad => ad.JuegoId == id).Select(a => new
+                        {
+                            a.AlquilerId,
+                            a.JuegoId,
+                            a.Juego.Nombre,
+                            a.Juego.Precio,
+                            NombreDirector = a.Juego.Director.Nombre,
+                            Plataformas = a.Juego.PlataformaJuegos.Select(pl => pl.Plataforma.Nombre)
+                        })
+                    })
+                    .Where(x => x.AlquilerDets.Select(ad => ad.JuegoId).Contains(id))
+                    .ToList();
+
+                var response = new
+                {
                     success = true,
                     data = query,
                 };
